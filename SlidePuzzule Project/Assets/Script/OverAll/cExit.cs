f715e823598928fa61ee8a0ca0978a8ab33385c0
mid-winter <roleplaying.1222@gmail.com>
2016-03-26 03:23:45 +0900

    もどるキーでアプリ終了と画面回転を固定化

---
+++ SlidePuzzule Project/Assets/Script/OverAll/cExit.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cExit : MonoBehaviour
+{
+    //初期化
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    //画面更新
+    void Update()
+    {
+        //もしプラットフォームがアンドロイドなら
+        if(Application.platform == RuntimePlatform.Android)
+        {
+            //エスケープキーでアプリ終了
+            if (Input.GetKey(KeyCode.Escape))
+            {
+                Application.Quit();
+                return;
+            }
+        }
+    }
+}
