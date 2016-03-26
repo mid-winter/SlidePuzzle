e5f11338511b166dc550c2f50b12246394b4ccdf
mid-winter <roleplaying.1222@gmail.com>
2016-03-27 02:03:50 +0900

    シーン追加 シーン移行をひとつのスクリプトにまとめた

---
+++ SlidePuzzule Project/Assets/Script/OverAll/cSceneMove.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class cSceneMove : MonoBehaviour {
+
+    ///<summary> タイトルへシーン遷移移行処理 </summary>
+    public void SceneMove(string SceneName)
+    {
+        SceneManager.LoadScene(SceneName);
+    }
+}
--- SlidePuzzule Project/Assets/Script/Title/cTitle.cs
+++ SlidePuzzule Project/Assets/Script/Title/cTitle.cs
@@ -1,29 +1,23 @@
 ﻿using UnityEngine;
-using System.Collections;
 using UnityEngine.SceneManagement;
 
 public class cTitle : MonoBehaviour
 {
-
-    ///<summary> タッチ </summary>
-    private Touch _touch;
+    ///<summary> シーン遷移 </summary>
+    private cSceneMove _sceneMove;
 
     ///<summary> 初期化 </summary>
     void Awake()
     {
+        _sceneMove = GetComponent<cSceneMove>();
     }
 
     ///<summary> 画面更新 </summary>
     void Update()
     {
-        _touch = Input.GetTouch(0);
-
         if (Input.touchCount > 0)
         {
-            if (_touch.phase == TouchPhase.Began)
-            {
-                SceneManager.LoadScene("MainMenu");
-            }
+            _sceneMove.SceneMove("MainMenu");
         }
     }
 }
